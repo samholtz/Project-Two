eb3a2b3094721c23fb579be3cc027552749fee8c
Luke Kennin <lkmeq@me.com>
2018-09-20 14:19:58 -0500

    handlebars/htmlpaths

--- Alpha/routes/htmlRoutes.js
+++ Alpha/routes/htmlRoutes.js
@@ -4,27 +4,32 @@ var path = require("path");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index");
   });
 
   // Route to the store page
   app.get("/store", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/store.html"));
+    res.render("store");
   });
 
   // race route loads race.html
   app.get("/race", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/race.html"));
+    res.render("race");
   });
 
   // petting zoo route loads pettingzoo.html
   app.get("/pettingzoo", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/pettingzoo.html"));
+    res.render("pettingzoo");
   });
 
   // trophy case route loads trophycase.html
   app.get("/trophycase", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/trophycase.html"));
+    res.render("trophycase");
+  });
+
+  //battle route leads to battle.html
+  app.get("/battle", function (req, res) {
+    res.render("battle");
   });
 
   // Render 404 page for any unmatched routes
